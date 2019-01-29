f52ab36aa0b39251bc8dbff933e4c8fa976a4eb6
Alexandra Palermo <alexmpalermo@gmail.com>
2019-01-29 18:32:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ def self.new_from_filename(filename)
   
   data = filename.split(" - ")
   artist_name = data[0]
-  name= data[1].slice! ".mp3"
+  name= data[1].chomp(".mp3") 
   
   
   song = self.new
